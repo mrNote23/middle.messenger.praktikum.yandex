4d999c5ce395808fab67b26bfdd86f59b791fa0f
mrNote23 <suvorov.av93@gmail.com>
2023-05-03 20:44:31 +0300

    x

--- src/core/Router.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-import Router from "./Router";
-
-describe("Router test", () => {
-  it("Переход на новую страницу должен менять состояние сущности history", () => {
-    console.log(Router);
-  });
-});
